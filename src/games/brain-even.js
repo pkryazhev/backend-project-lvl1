42ba06c7e1d709fe8765cfff19e98662ac06bcd4
Pavel Kryazhev <kryazhevpavel94@gmail.com>
2021-08-03 16:33:09 +0300

    isEven ref

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -3,13 +3,13 @@ import play, { roundQuantity } from '../index.js';
 
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (number) => (number % 2 === 0 ? 'yes' : 'no');
+const isEven = (number) => number % 2 === 0;
 
 const generateGameData = () => {
   const result = [];
   for (let i = 0; i < roundQuantity; i += 1) {
     const number = generateNumber(1, 100);
-    const rightAnswer = isEven(number);
+    const rightAnswer = isEven(number) ? 'yes' : 'no';
     const question = String(number);
     result.push([rightAnswer, question]);
   }
